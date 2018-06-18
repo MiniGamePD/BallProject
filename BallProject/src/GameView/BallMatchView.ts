6169ae1c0e21299edc4704eaca8c96fb9205ef9d
Dennislai00 <34063969+Dennislai00@users.noreply.github.com>
2018-06-18 21:26:44 +0800

    获得道具的提示

--- BallProject/src/GameView/BallMatchView.ts
+++ BallProject/src/GameView/BallMatchView.ts
@@ -1,8 +1,8 @@
 class BallMatchView extends GameView
 {
-    private hud:MatchHUD;
-    private playerLv:egret.TextField;
-    private battleGround:egret.DisplayObjectContainer;
+    private hud: MatchHUD;
+    private playerLv: egret.TextField;
+    private battleGround: egret.DisplayObjectContainer;
 
     public CreateView(): void
     {
@@ -17,14 +17,16 @@ class BallMatchView extends GameView
     }
 
     private RegisterEvent(): void
-	{
-		GameMain.GetInstance().AddEventListener(BallEmitterLevelUpEvent.EventName, this.OnBallEmitterLevelUpEvent, this);
-	}
+    {
+        GameMain.GetInstance().AddEventListener(BallEmitterLevelUpEvent.EventName, this.OnBallEmitterLevelUpEvent, this);
+        GameMain.GetInstance().AddEventListener(SpecialBoxEliminateEvent.EventName, this.OnSpecialBoxEliminateEvent, this);
+    }
 
-	private UnRegisterEvent(): void
-	{
-		GameMain.GetInstance().RemoveEventListener(BallEmitterLevelUpEvent.EventName, this.OnBallEmitterLevelUpEvent, this);
-	}
+    private UnRegisterEvent(): void
+    {
+        GameMain.GetInstance().RemoveEventListener(BallEmitterLevelUpEvent.EventName, this.OnBallEmitterLevelUpEvent, this);
+        GameMain.GetInstance().RemoveEventListener(SpecialBoxEliminateEvent.EventName, this.OnSpecialBoxEliminateEvent, this);
+    }
 
     public UpdateView(deltaTime: number): void 
     {
@@ -55,7 +57,7 @@ class BallMatchView extends GameView
         // this.playerLv.touchEnabled = true;
         // this.playerLv.addEventListener(egret.TouchEvent.TOUCH_TAP, this.GameOver, this);
     }
-    
+
     private OnBallEmitterLevelUpEvent(event: BallEmitterLevelUpEvent)
     {
         this.playerLv.text = "Lv. " + event.curLevel;
@@ -69,14 +71,14 @@ class BallMatchView extends GameView
         this.addChild(this.battleGround);
     }
 
-    public GetBattleGround():egret.DisplayObjectContainer
+    public GetBattleGround(): egret.DisplayObjectContainer
     {
         return this.battleGround;
     }
 
     private GameOver()
     {
-        let event = new GameOverEvent();            
+        let event = new GameOverEvent();
         GameMain.GetInstance().DispatchEvent(event);
     }
 
@@ -102,8 +104,72 @@ class BallMatchView extends GameView
         this.hud = null;
     }
 
-    public SetLevel(level:number)
+    public SetLevel(level: number)
     {
         this.playerLv.text = "Lv. " + level;
+    }
+
+    private OnSpecialBoxEliminateEvent(evt: SpecialBoxEliminateEvent): void
+	{
+		if (evt != null)
+		{
+			if (evt.boxType == BoxType.SixMulDir)
+			{
+				this.ShowTips("变身 5秒", 0x3562ec);
+			}
+			else if (evt.boxType == BoxType.FireUp)
+			{
+				this.ShowTips("全力开火 5秒", 0xd6340a);
+			}
+			else if (evt.boxType == BoxType.LevelUp)
+			{
+				this.ShowTips("炮台升级", 0x59d61b);
+			}
+            else if (evt.boxType == BoxType.Pause)
+			{
+				this.ShowTips("定时 5秒", 0x6726a5);
+			}
+		}
+	}
+
+
+    public ShowTips(tipString: string, color: number)
+    {
+        var tips: egret.TextField;
+        tips = new egret.TextField();
+        tips.text = tipString;
+        tips.size = 30;
+        tips.width = 200;
+        tips.height = 60;
+        tips.anchorOffsetX = tips.width / 2;
+        tips.anchorOffsetY = tips.height / 2;
+        tips.textAlign = "center";
+        tips.bold = true;
+        tips.x = GameMain.GetInstance().GetStageWidth() / 2;
+        tips.y = GameMain.GetInstance().GetStageHeight() / 2 - 50;
+        tips.textColor = color;
+        this.addChild(tips);
+
+        var moveParam = new PaMovingParam()
+        moveParam.displayObj = tips;
+        moveParam.duration = 1000;
+        moveParam.targetPosX = tips.x;
+        moveParam.targetPosY = tips.y - 40;
+        moveParam.needRemoveOnFinish = true;
+        var moveEvent = new PlayProgramAnimationEvent();
+        moveEvent.param = moveParam;
+        GameMain.GetInstance().DispatchEvent(moveEvent);
+
+
+        var alphaParam = new PaAlphaLoopParam();
+        alphaParam.displayObj = tips;
+        alphaParam.duration = 1000;
+        alphaParam.interval = 500;
+        alphaParam.startAlpha = 0.5
+        alphaParam.endAlpha = 1
+        alphaParam.reverse = true;
+        var alphaEvent = new PlayProgramAnimationEvent();
+        alphaEvent.param = alphaParam;
+        GameMain.GetInstance().DispatchEvent(alphaEvent);
     }
 } 
