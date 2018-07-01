2452ffcd9423dcfacb68dc32a1848fc57656caf8
UnknowPlayer <>
2018-07-01 23:56:32 +0800

    排行榜新资源合入

--- BallProject/src/GameView/LobbyView.ts
+++ BallProject/src/GameView/LobbyView.ts
@@ -165,8 +165,8 @@ class LobbyView extends GameView
 
         //入口
         this.rankButton = this.mResModule.CreateBitmapByName("pd_res_json.rankEntry");
-        this.rankButton.width *= 0.4 * adaptFactor;
-        this.rankButton.height *= 0.4 * adaptFactor;
+        this.rankButton.width *= adaptFactor;
+        this.rankButton.height *= adaptFactor;
         this.rankButton.anchorOffsetX = this.rankButton.width / 2;
         this.rankButton.anchorOffsetY = this.rankButton.height / 2;
         this.rankButton.x = 430 * adaptFactor;
